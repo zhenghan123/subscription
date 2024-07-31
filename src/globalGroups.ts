8d59d65ab37dd568728507baec4c557c4a7c6cb8
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-07-31 19:56:38 +0800

    Update globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -1,3 +1,379 @@
 import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
-export default defineGkdGlobalGroups([]);
+export default defineGkdGlobalGroups([
+ {
+    key: 0,
+    name: '开屏广告',
+    order: OPEN_AD_ORDER,
+    actionMaximum: 2,
+    matchTime: 8000,
+    resetMatch: 'app',
+    actionCdKey: 0,
+    actionMaximumKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: `[childCount<7] > ${COMMON_PREFIX}[(text.length<10&&(text*="跳过"||text*="跳過"||text~="(?is).*skip.*"))||id~="(?is).*tt_splash_skip_btn"||vid~="(?is).*skip.*"||(vid~="(?is).*count.*"&&vid~="(?is).*down.*"&&vid!~="(?is).*load.*"&&vid!~="(?is).*hour.*"&&vid!~="(?is).*minute.*"&&vid!~="(?is).*add.*"&&vid!~="(?is).*ead.*")||desc*="跳过"||desc~="(?is).*skip.*"]`,
+      },
+      {
+        key: 1,
+        matches:
+          'FrameLayout[visibleToUser=true][height>0&&width>0][childCount>=2] > FrameLayout[visibleToUser=true][height>0&&width>0][childCount>2] > @View[clickable=true][height>0&&width>0][visibleToUser=true] + TextView[clickable=true][height>0&&width>0][visibleToUser=true]', // 字节 SDK
+      },
+      {
+        key: 2,
+        fastQuery: true,
+        matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+      },
+      {
+        key: 3,
+        matches:
+          '[childCount=0][visibleToUser=true][((text*="跳过" || text*="跳過" || text~="(?is).*skip.*") && text.length<10) || ((desc*="跳过" || desc*="跳過" || desc~="(?is).*skip.*") && desc.length<10) || id~="(?is).*tt_splash_skip_btn" || vid~="(?is).*skip.*" || (vid~="(?is).*count.*" && vid~="(?is).*down.*" && vid!~="(?is).*load.*" && vid!~="(?is).*time.*" && vid!~="(?is).*hour.*" && vid!~="(?is).*minute.*" && vid!~="(?is).*second.*" && vid!~="(?is).*add.*" && vid!~="(?is).*ead.*" && text!~="([01]?[0-9]|2[0-3])[:：][0-5][0-9]")]',
+      },
+      {
+        key: 4,
+        fastQuery: true,
+        matches:
+          'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][visibleToUser=true] + TextView[text=null] <<n [id="android:id/content"]',
+      },
+    ],
+    apps: [...appList.openAdBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.openAdWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 1,
+    name: '更新提示',
+    enable: false,
+    order: UPDATE_ORDER,
+    actionMaximum: 2,
+    matchTime: 10000,
+    resetMatch: 'app',
+    actionCdKey: 0,
+    actionMaximumKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: [
+          `${COMMON_PREFIX}[(text*="内测"||text*="测试版"||text*="新版"||text*="更新"||text*="升级"||text*="体验"||text*="內測"||text*="測試版"||text*="升級"||text*="體驗"||text~="(?is).*Update.*"||text~="(?is).*Upgrade.*"||text~="(?is).*Experience.*")&&text!*="自动"&&text!*="自動"&&(text!*="成功"&&text!*="失败"&&text!*="失敗"&&(text!*="已"||text*="已知")&&text!*="检查更新")]`,
+          `${COMMON_PREFIX}[(((text*="立即"||text*="马上"||text*="并"||text*="现在"||text*="立刻"||text*="抢先")&&(text*="更新"||text*="下载"||text*="安装"||text*="升级"||text*="查看"||text*="体验"))||text="更新"||text="下载"||text="安装"||text="升级"||text="确定"||text="好,升级")&&text.length<6]`,
+          `${NEGATION_PART_RULE_TEXT}`,
+        ],
+      },
+      {
+        key: 1,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="内测"||desc*="测试版"||desc*="新版"||desc*="更新"||desc*="升级"||desc*="体验"||desc*="內測"||desc*="測試版"||desc*="升級"||desc*="體驗"||desc~="(?is).*Update.*"||desc~="(?is).*Upgrade.*"||desc~="(?is).*Experience.*")&&desc!*="自动"&&desc!*="自動"&&(desc!*="成功"&&desc!*="失败"&&desc!*="失敗"&&(desc!*="已"||desc*="已知")&&desc!*="检查更新")]`,
+          `${COMMON_PREFIX}[(((desc*="立即"||desc*="马上"||desc*="并"||desc*="现在"||desc*="立刻"||desc*="抢先")&&(desc*="更新"||desc*="下载"||desc*="安装"||desc*="升级"||desc*="查看"||desc*="体验"))||desc="更新"||text="下载"||desc="安装"||desc="升级"||desc="确定"||desc="好,升级")&&desc.length<6]`,
+          `${NEGATION_PART_RULE_DESC}`,
+        ],
+      },
+      {
+        key: 2,
+        matches: [
+          `${COMMON_PREFIX}[(text*="内测"||text*="测试版"||text*="新版"||text*="更新"||text*="升级"||text*="体验"||text*="內測"||text*="測試版"||text*="升級"||text*="體驗"||text~="(?is).*Update.*"||text~="(?is).*Upgrade.*"||text~="(?is).*Experience.*")&&text!*="自动"&&text!*="自動"&&(text!*="成功"&&text!*="失败"&&text!*="失敗"&&(text!*="已"||text*="已知")&&text!*="检查更新")]`,
+          `${COMMON_PREFIX}[(((text*="立即"||text*="马上"||text*="并"||text*="现在"||text*="立刻"||text*="抢先")&&(text*="更新"||text*="下载"||text*="安装"||text*="升级"||text*="查看"||text*="体验"))||text="更新"||text="下载"||text="安装"||text="升级"||text="确定"||text="好,升级")&&text.length<6]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 3,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="内测"||desc*="测试版"||desc*="新版"||desc*="更新"||desc*="升级"||desc*="体验"||desc*="內測"||desc*="測試版"||desc*="升級"||desc*="體驗"||desc~="(?is).*Update.*"||desc~="(?is).*Upgrade.*"||desc~="(?is).*Experience.*")&&desc!*="自动"&&desc!*="自動"&&(desc!*="成功"&&desc!*="失败"&&desc!*="失敗"&&(desc!*="已"||desc*="已知")&&desc!*="检查更新")]`,
+          `${COMMON_PREFIX}[(((desc*="立即"||desc*="马上"||desc*="并"||desc*="现在"||desc*="立刻"||desc*="抢先")&&(desc*="更新"||desc*="下载"||desc*="安装"||desc*="升级"||desc*="查看"||desc*="体验"))||desc="更新"||text="下载"||desc="安装"||desc="升级"||desc="确定"||desc="好,升级")&&desc.length<6]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 4,
+        matches: [
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][(text*="内测" || text*="测试版" || text*="新版" || text*="更新" || text*="升级" || text*="体验" || text*="內測" || text*="測試版" || text*="升級" || text*="體驗" || text*="Update" || text*="Upgrade" || text*="Experience") && text!*="自动" && text!*="自動" && (text!*="成功" && text!*="失败" && text!*="失敗" && (text!*="已" || text*="已知") && text!*="检查更新")]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][(((text*="立即" || text*="马上" || text*="并" || text*="现在" || text*="立刻" || text*="抢先" || text*="在线" || text*="提醒")&&(text*="更新" || text*="下载" || text*="安装" || text*="升级" || text*="查看" || text*="体验")) || text="更新" || text="下载" || text="安装" || text="升级" || text="确定" || text="好,升级") && text.length<6]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][text^="不再" || text$="再说" || text$="拒绝" || text$="再想想" || text^="忽略" || text^="暂不" || text^="放弃" || text^="取消" || text$="不要" || text$="再說" || text$="暫不" || text$="拒絕" || text^="稍后" || text$="Later" || text^="Ignore" || text^="Not now" || text^="Cancel" || vid^="close" || vid^="Close" || vid^="closeIv" || vid*="_close" || vid*="_Close" || vid$="close" || vid$="Close" || vid$="cancel" || vid$="Cancel" || vid$="cancle" || vid$="Cancle"]',
+        ],
+      },
+    ],
+    apps: [...appList.updateBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.updateWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 2,
+    name: '青少年模式',
+    enable: false,
+    order: YOUNG_ORDER,
+    actionMaximum: 2,
+    matchTime: 10000,
+    resetMatch: 'app',
+    actionCdKey: 0,
+    actionMaximumKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: [
+          `${COMMON_PREFIX}[(text*="未成年"||text*="儿童"||text*="青少年"||text*="守护")&&text*="模式"&&text.length<15]`,
+          `${COMMON_PREFIX}[(text*="开启"||text*="进入"||text*="设置"||text*="设计"||text*="查看")&&(text*="模式"||text*="去")&&text.length<15]`,
+          `${NEGATION_PART_RULE_TEXT}`,
+        ],
+      },
+      {
+        key: 1,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="未成年"||desc*="儿童"||desc*="青少年"||desc*="守护")&&desc*="模式"&&desc.length<15]`,
+          `${COMMON_PREFIX}[(desc*="开启"||desc*="进入"||desc*="设置"||desc*="设计"||desc*="查看")&&(desc*="模式"||desc*="去")&&desc.length<15]`,
+          `${NEGATION_PART_RULE_DESC}`,
+        ],
+      },
+      {
+        key: 2,
+        matches: [
+          `${COMMON_PREFIX}[(text*="未成年"||text*="儿童"||text*="青少年"||text*="守护")&&text*="模式"&&text.length<15]`,
+          `${COMMON_PREFIX}[(text*="开启"||text*="进入"||text*="设置"||text*="设计"||text*="查看")&&(text*="模式"||text*="去")&&text.length<15]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 3,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="未成年"||desc*="儿童"||desc*="青少年"||desc*="守护")&&desc*="模式"&&desc.length<15]`,
+          `${COMMON_PREFIX}[(desc*="开启"||desc*="进入"||desc*="设置"||desc*="设计"||desc*="查看")&&(desc*="模式"||desc*="去")&&desc.length<15]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 4,
+        matches: [
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][((text*="青少年" || text*="未成年" || text*="儿童") && (text*="模式" || text*="守护")) && text.length<15 || ((desc*="青少年" || desc*="未成年" || desc*="儿童") && (desc*="模式" || desc*="守护")) && desc.length<15]',
+          '[name!$=".CheckBox"][childCount=0][visibleToUser=true][(text*="知道了" || text*="关闭" || text*="我已知晓" || text*="已满") && text.length<15 || (desc*="知道了" || desc*="关闭" || desc*="我已知晓" || desc*="已满") && desc.length<15]',
+        ],
+      },
+    ],
+    apps: [...appList.yongBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.yongWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 3,
+    name: '全屏广告',
+    enable: false,
+    order: FULLSCREEN_AD_ORDER,
+    matchTime: 10000,
+    resetMatch: 'activity',
+    actionCdKey: 0,
+    rules: [
+      {
+        key: 0,
+        name: '快手SDK-类型1',
+        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=1][clickable=true] > ImageView${COMMON_PREFIX}`,
+      },
+      {
+        key: 1,
+        name: '快手SDK-类型2',
+        matches: `[text="广告"] <<n ViewGroup >n ViewGroup[childCount=4][clickable=true] > [text="跳过"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 2,
+        name: '快手SDK-类型3',
+        matches: `[text="广告"]  <<n ViewGroup +2 ViewGroup[childCount=3][checked=false] >n ImageView${COMMON_PREFIX}`,
+      },
+      {
+        key: 3,
+        name: '字节SDK-类型1',
+        matches: `[id~="(?is).*tt_reward_full_count_down_after_close"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 4,
+        name: '字节SDK-类型2',
+        matches: `[vid~="(?is).*ad_flag_source_layout"] + [vid~="(?is).*iv_close"]${COMMON_PREFIX}`,
+      },
+      {
+        key: 5,
+        name: '美数SDK-类型1',
+        matches: `[vid~="(?is).*ms_activity_sdk_interstitial_cacel"]${COMMON_PREFIX}`,
+      },
+    ],
+    apps: [...appList.fullAdBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.fullAdWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 4,
+    name: '局部广告',
+    enable: false,
+    order: PARTIAL_AD_ORDER,
+    matchTime: 10000,
+    resetMatch: 'activity',
+    actionCdKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: `[(((vid~="(?is)ad.*"||vid~="(?is).*ad")&&vid*="_")||vid~="adIv"||vid~="(?is).*_ad_.*")&&(vid!~="(?is).*load.*")&&(vid!~="(?is).*read.*")&&(vid!~="(?is).*add.*")&&(vid!~="(?is).*ead.*")][height>0&&width>0] <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 1,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 2,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 3,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_TEXT}`,
+      },
+      {
+        key: 4,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 5,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 6,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_DESC}`,
+      },
+      {
+        key: 7,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 8,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 9,
+        matches: `${COMMON_PREFIX}${PA_commonTextPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 10,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 11,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+      {
+        key: 12,
+        matches: `${COMMON_PREFIX}${PA_commonDescPatterns} <<n * <n * > * >n ${NEGATION_PART_RULE_BUTTON}`,
+      },
+    ],
+    apps: [...appList.partialAdBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.partialAdWhiteListAppIDs].map((id) => ({
+          id,
+          enable: true,
+        })),
+      ),
+  },
+  {
+    key: 5,
+    name: '评价提示',
+    enable: false,
+    order: REVIEW_PROMPT_ORDER,
+    actionMaximum: 2,
+    matchTime: 10000,
+    resetMatch: 'app',
+    actionCdKey: 0,
+    actionMaximumKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: [
+          `${COMMON_PREFIX}[((text*="给"||text*="赏"||text*="来个"||text*="一下"||text*="app"||text*="喜欢")&&(text*="好评"||text*="评分"||text*="评价"||text*="鼓励"||text*="Rate"||text*="吗？"))&&(text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))||name$=".RatingBar"]`,
+          `${COMMON_PREFIX}[((text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
+          `${NEGATION_PART_RULE_TEXT}`,
+        ],
+      },
+      {
+        key: 1,
+        matches: [
+          `${COMMON_PREFIX}[((desc*="给"||desc*="赏"||desc*="来个"||desc*="一下"||desc*="app"||desc*="喜欢")&&(desc*="好评"||desc*="评分"||desc*="评价"||desc*="鼓励"||desc*="Rate"||desc*="吗？"))&&(desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))||name$=".RatingBar"]`,
+          `${COMMON_PREFIX}[((desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
+          `${NEGATION_PART_RULE_DESC}`,
+        ],
+      },
+      {
+        key: 2,
+        matches: [
+          `${COMMON_PREFIX}[((text*="给"||text*="赏"||text*="来个"||text*="一下"||text*="app"||text*="喜欢")&&(text*="好评"||text*="评分"||text*="评价"||text*="鼓励"||text*="Rate"||text*="吗？"))&&(text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))||name$=".RatingBar"]`,
+          `${COMMON_PREFIX}[((text*="好评"||text*="鼓励一下"||text*="评价"||text*="评分")||(text="确定"||text="提交"||text="满意"||text~="RATE NOW"||text~="(?is).*Love it.*"))&&text.length<9]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 3,
+        matches: [
+          `${COMMON_PREFIX}[((desc*="给"||desc*="赏"||desc*="来个"||desc*="一下"||desc*="app"||desc*="喜欢")&&(desc*="好评"||desc*="评分"||desc*="评价"||desc*="鼓励"||desc*="Rate"||desc*="吗？"))&&(desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))||name$=".RatingBar"]`,
+          `${COMMON_PREFIX}[((desc*="好评"||desc*="鼓励一下"||desc*="评价"||desc*="评分")||(desc="确定"||desc="提交"||desc="满意"||desc~="RATE NOW"||desc~="(?is).*Love it.*"))&&desc.length<9]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+    ],
+    apps: [...appList.reviewBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.reviewWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 6,
+    name: '通知提示',
+    enable: false,
+    order: NOTIFICATION_PROMPT_ORDER,
+    matchTime: 10000,
+    resetMatch: 'app',
+    actionCdKey: 0,
+    rules: [
+      {
+        key: 0,
+        matches: [
+          `${COMMON_PREFIX}[(text*="可能错过"||text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="获得"||text*="订阅"||text*="接收"||text~="(?is).*Turn on.*"||text~="(?is).*Enable.*")&&(text*="通知"||text*="推送"||text*="消息"||text~="(?is).*notification.*")&&(text!*="定位"&&text!*="位置"&&text!~="(?is).*location.*"&&(text!*="权限"||text*="通知权限"||text*="通知栏权限")&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]`,
+          `${COMMON_PREFIX}[(((text*="打开"||text*="開啟"||text*="开启"||text*="设置"||text*="允许")&&(text*="通知"||text*="推送"))||text="是"||text="好"||text="启用"||text="确认"||text="确定"||text="同意"||text="开启"||text="设置"||text="允许"||text="好的"||((text*="去"||text*="立即"||text*="立刻"||text*="马上"||text*="前往"||text*="现在")&&(text*="开启"||text*="设置"||text*="授权"))||text="尝试一下"||text="开启设置"||text="立即打开"||text~="Authorize"||text~="Let’s go")&&text.length<10]`,
+          `${NEGATION_PART_RULE_TEXT}`,
+        ],
+      },
+      {
+        key: 1,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="可能错过"||desc*="是否允许"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="获得"||desc*="订阅"||desc*="接收"||desc~="(?is).*Turn on.*"||desc~="(?is).*Enable.*")&&(desc*="通知"||desc*="推送"||desc*="消息"||desc~="(?is).*notification.*")&&(desc!*="定位"&&desc!*="位置"&&desc!~="(?is).*location.*"&&(desc!*="权限"||desc*="通知权限"||desc*="通知栏权限")&&desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))]`,
+          `${COMMON_PREFIX}[(((desc*="打开"||desc*="開啟"||desc*="开启"||desc*="设置"||desc*="允许")&&(desc*="通知"||desc*="推送"))||desc="是"||desc="好"||desc="启用"||desc="确认"||desc="确定"||desc="同意"||desc="开启"||desc="设置"||desc="允许"||desc="好的"||((desc*="去"||desc*="立即"||desc*="立刻"||desc*="马上"||desc*="前往"||desc*="现在")&&(desc*="开启"||desc*="设置"||desc*="授权"))||desc="尝试一下"||desc="开启设置"||desc="立即打开"||desc~="Authorize"||desc~="Let’s go")&&desc.length<10]`,
+          `${NEGATION_PART_RULE_DESC}`,
+        ],
+      },
+      {
+        key: 2,
+        matches: [
+          `${COMMON_PREFIX}[(text*="可能错过"||text*="是否允许"||text*="申请"||text*="开启"||text*="打开"||text*="获取"||text*="获得"||text*="订阅"||text*="接收"||text~="(?is).*Turn on.*"||text~="(?is).*Enable.*")&&(text*="通知"||text*="推送"||text*="消息"||text~="(?is).*notification.*")&&(text!*="定位"&&text!*="位置"&&text!~="(?is).*location.*"&&(text!*="权限"||text*="通知权限"||text*="通知栏权限")&&text!*="成功"&&text!*="失败"&&(text!*="已"||text*="已知"))]`,
+          `${COMMON_PREFIX}[(((text*="打开"||text*="開啟"||text*="开启"||text*="设置"||text*="允许")&&(text*="通知"||text*="推送"))||text="是"||text="好"||text="启用"||text="确认"||text="确定"||text="同意"||text="开启"||text="设置"||text="允许"||text="好的"||((text*="去"||text*="立即"||text*="立刻"||text*="马上"||text*="前往"||text*="现在")&&(text*="开启"||text*="设置"||text*="授权"))||text="尝试一下"||text="开启设置"||text="立即打开"||text~="Authorize"||text~="Let’s go")&&text.length<10]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+      {
+        key: 3,
+        matches: [
+          `${COMMON_PREFIX}[(desc*="可能错过"||desc*="是否允许"||desc*="申请"||desc*="开启"||desc*="打开"||desc*="获取"||desc*="获得"||desc*="订阅"||desc*="接收"||desc~="(?is).*Turn on.*"||desc~="(?is).*Enable.*")&&(desc*="通知"||desc*="推送"||desc*="消息"||desc~="(?is).*notification.*")&&(desc!*="定位"&&desc!*="位置"&&desc!~="(?is).*location.*"&&(desc!*="权限"||desc*="通知权限"||desc*="通知栏权限")&&desc!*="成功"&&desc!*="失败"&&(desc!*="已"||desc*="已知"))]`,
+          `${COMMON_PREFIX}[(((desc*="打开"||desc*="開啟"||desc*="开启"||desc*="设置"||desc*="允许")&&(desc*="通知"||desc*="推送"))||desc="是"||desc="好"||desc="启用"||desc="确认"||desc="确定"||desc="同意"||desc="开启"||desc="设置"||desc="允许"||desc="好的"||((desc*="去"||desc*="立即"||desc*="立刻"||desc*="马上"||desc*="前往"||desc*="现在")&&(desc*="开启"||desc*="设置"||desc*="授权"))||desc="尝试一下"||desc="开启设置"||desc="立即打开"||desc~="Authorize"||desc~="Let’s go")&&desc.length<10]`,
+          `${NEGATION_PART_RULE_BUTTON}`,
+        ],
+      },
+    ],
+    apps: [...appList.notificationBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.notificationWhiteListAppIDs].map((id) => ({
+          id,
+          enable: true,
+        })),
+      ),
+  },
+]);
