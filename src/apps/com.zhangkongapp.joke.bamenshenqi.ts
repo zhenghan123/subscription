cb31f98729596aefc3b95a814cb77459f7b888d1
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-08-18 18:54:25 +0800

    Create com.zhangkongapp.joke.bamenshenqi.ts

---
+++ src/apps/com.zhangkongapp.joke.bamenshenqi.ts
@@ -0,0 +1,20 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.zhangkongapp.joke.bamenshenqi',
+  name: '八门神器',
+  groups: [
+    {
+      name: '更新弹窗',
+      key: 0,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['[name="android.widget.ImageView"]'],
+        },
+      ],
+    },
+  ],
+});
