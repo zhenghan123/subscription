78665b1ac4923c4bf40a8f5f1b1824700b4fa397
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-08-20 16:06:59 +0800

    Update com.duowan.kiwi.ts

--- src/apps/com.duowan.kiwi.ts
+++ src/apps/com.duowan.kiwi.ts
@@ -1,7 +1,7 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'com.zhangkongapp.joke.bamenshenqi',
+  id: 'com.duowan.kiwi',
   name: '虎牙直播',
   groups: [
     {
