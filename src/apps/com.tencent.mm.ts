ed23cd13af8f8ccfeef7d4dfbcdcaf00463f89d3
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-08-26 10:47:19 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,5 +3,18 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+    {
+      name: '继续访问',
+      key: 0,
+      actionMaximum: 100,
+      matchTime: 890000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['[text="继续访问"]'],
+        },
+      ],
+    },
+  ],
 });
