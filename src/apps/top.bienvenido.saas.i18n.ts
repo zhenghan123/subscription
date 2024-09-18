fccb254abac1d176b76feecf70355d8f1d41ae62
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-09-18 09:44:26 +0800

    Create top.bienvenido.saas.i18n.ts

---
+++ src/apps/top.bienvenido.saas.i18n.ts
@@ -0,0 +1,20 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'top.bienvenido.saas.i18n',
+  name: '元萝卜',
+  groups: [
+    {
+      name: '开局弹窗',
+      key: 0,
+      actionMaximum: 1,
+      matchTime: 10000000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['[id="com.sfacg:id/tv_jump"]'],
+        },
+      ],
+    },
+  ],
+});
